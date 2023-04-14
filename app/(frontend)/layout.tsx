3f5557849460f37675d4f1c84fdd71f3f2c59662
Nico Setiawan <nicoesetiawan@gmail.com>
2023-04-14 15:32:35 +0700

    Add CMS Sanity

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import "../global.css";
+import "../globals.css";
 import Footer from "@/components/Footer";
 import { Providers } from "@/components/Providers";
 
